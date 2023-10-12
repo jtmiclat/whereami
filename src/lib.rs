d1a52f9c2cd80cf4d9d9083c744620e726e67015
Jt Miclat <jtmiclat@gmail.com>
2023-10-12 20:38:44 +0800

    Make return string JSON valid

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,6 @@ async fn main(req: Request, _env: Env, _ctx: Context) -> Result<Response> {
         let city = req.cf().city().unwrap_or_default();
         let country_alpha2 = req.cf().country().unwrap_or_default();
         let country = Country::from_alpha2(country_alpha2).unwrap().long_name;
-        Response::ok(format!("{}, {}", city, country))
+        Response::ok(format!("\"{}, {}\"", city, country))
     }
 }
